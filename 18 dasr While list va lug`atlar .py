f6118b7636cafc67284c03f24634b7f172080ca7
mastermirzodev <xushbakovmuzaffar@gmail.com>
2024-02-06 21:00:28 +0500

    Create 18 dasr While list va lug`atlar .py

---
+++ 18 dasr While list va lug`atlar .py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 19:39:32 2024
+
+@author: mirzo
+"""
+
+# print("Yaqin do`slarimizning ro`yxatini tuzamiz")
+# ismlar = []
+# n=1
+# while True:
+#     savol = F'{n} -do`stingizning ismini kiriting:'
+#     ism = input(savol)
+#     ismlar.append(ism)
+#     takrorlash = input("Yana ism qo`shasizmi? (ha/yo`q)")
+#     n+=1
+#     if takrorlash != 'ha':
+#         break
+    
+# print("Do`slaringiz ro`yxati")
+# for ism in ismlar:
+#     print(ism.title())
+
+# print("Yaqin do`slarimizning ro`yxatini tuzamiz")
+# dostlar = {}
+# ishora = True
+# while ishora:
+#     ism = input("Do`stingizning ismini kiriting:\n")
+#     yosh = input(f"{ism.title()}ning yosini kiriting:\n")
+#     dostlar[ism] = int(yosh)
+    
+#     javob = input("Yana ma`mumot kiritasizmi? ha/yoq")
+#     if javob == "yoq":
+#         ishora = False
+# for ism, yosh in dostlar.items():
+#     print(f"{ism.title()} {yosh} yoshda")
+    
+# cars = ['lachetti', 'nexia', 'toyota', 'bmw', 'audi', 'nexia', 'malibu', 'nexia']
+# while 'nexia' in cars:
+#     cars.remove('nexia')
+# print(cars)
+
+# talabalar = ['hasan', 'husan', 'olim', 'anton', 'botir', 'alyosh']
+# baholangan_talabalar = {}
+# while talabalar:
+#     talaba = talabalar.pop()
+#     baho = input(f"{talaba.title()}ning bahosini kiriting:")
+#     print(f"{talaba.title()} baholandi")
+#     baholangan_talabalar[talaba] = int(baho)
+
+
+# mahsulot = []
+# while True:
+#     savol = f'mahsulot nomini kiriting:'
+#     n = input(savol)
+#     mahsulot.append(n)
+#     takror = input(f"yana mahsulot qo`shasizmi? ha/yoq ")
+#     if takror != 'ha':
+#         break
+# print(f"siz tanlagan mahsulotlar {mahsulot}")
+    
+# mahsulotlar = {}
+# while True:
+#     nom= input(f'mahsulot nomini kiriting')
+#     narx = input(f"{nom}ning narxini kiriting")
+#     mahsulotlar[nom] = int(narx)
+    
+#     n = input(f"yana mahsulot kiritasizmi? ha/yuq")
+#     if n != 'ha':
+#         break
+# print(mahsulotlar)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
